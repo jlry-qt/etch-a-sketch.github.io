e446db7cca1f4bddfeb5e5a84f34fbe5098264b7
jlry <personal.jlry@gmail.com>
2024-03-13 15:18:59 +0800

    Added raindbow draw mode and improved drawHandler function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-const gridContainer = document.querySelector('#grid-container')
+const gridContainer = document.querySelector('#grid-container');
+const optionsContainer = document.querySelector('#options-container');
+const drawModes = document.querySelector('#draw-modes');
 const clearButton = document.querySelector('#clear-button');
 
-clearButton.addEventListener('click', clearHandler);
+clearButton.addEventListener('click', clearGrid);
 
 document.addEventListener('dragstart', event => {
     event.preventDefault();
@@ -25,12 +27,21 @@ function createGrid(){
 }
 
 
-
 function drawHandler(){
     let mouseIsDown = false;
+    let mode = 'normal-draw';
+
+    drawModes.addEventListener('click', event => {
+        mode = event.target.id;
+    })
 
     gridContainer.addEventListener('mousedown', event => {
-        event.target.style.background = 'black';
+        if (event.target.style.background != ''){
+            return
+        }
+
+        event.target.style.background = (mode === 'normal-draw') ? 'black' :
+                                                                    drawRandomColor();
         mouseIsDown = true;
     });
 
@@ -38,35 +49,52 @@ function drawHandler(){
         mouseIsDown = false;
     })
 
-
     gridContainer.addEventListener('mouseover', event => {
         if (mouseIsDown){
-            event.target.style.background = 'black';
+            if (event.target.style.background != ''){
+                return
+            }
+
+            if (mode === 'normal-draw') {
+                event.target.style.background = 'black';
+            } else if (mode === 'rainbow-draw'){
+                event.target.style.background = drawRandomColor();
+            }
         } else {
-            // Shadow effect when hovering.
-            event.target.style.background = (event.target.style.background === 'black') ?
-                                            'black':
-                                            '#dedddc';
+            // This is for the "shadow effect"
+            event.target.classList.add('cursor-pointed');
         }
     })
-    
+
     gridContainer.addEventListener('mouseout', event => {
-        event.target.style.background = (event.target.style.background === 'black') ?
-                                        'black':
-                                        '';
+        event.target.classList.remove('cursor-pointed');
     })
+
+
+
 }
 
 
-/* 
-LISTEN for click event
-FIND ALL colored square
-TURN them into white
-*/
-function clearHandler(){
+function clearGrid(){
     document.querySelectorAll('.grid-square').forEach(gridSquare => {
         gridSquare.style.background = '';
     });
+}
+
+function drawRandomColor(){
+    const redValue = Math.floor(Math.random() * 255) + 1;
+    const blueValue = Math.floor(Math.random() * 255) + 1;
+    const greenValue = Math.floor(Math.random() * 255) + 1;
+
+    const RGB = '#' +
+                redValue.toString(16) +
+                blueValue.toString(16) +
+                greenValue.toString(16);
+
+    if (RGB === '#cecccc'){
+        return drawRandomColor();
+    }
+    return RGB;
 }
 
 createGrid();
